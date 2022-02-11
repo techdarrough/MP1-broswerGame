bb11d54826f4b89287bb7b9429cff0070609b4b0
Preston Darrough <techdarrough@gmail.com>
2022-02-10 22:35:36 -0800

    fourth commit: start to build deck in javascript

---
+++ assets/js/blackJackapp.js
@@ -0,0 +1,10 @@
+
+// code inspiration from that software dude content 6417 
+const cards = {
+    suits: ["Spades", "Clubs", "Hearts", "Diamonds"],
+    value: [[2-10], "J", "Q", "K", "A" ],
+}
+let deck = new Array();
+console.log(deck)
+console.log(cards)
+
