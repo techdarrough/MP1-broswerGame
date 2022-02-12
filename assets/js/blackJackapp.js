50f06e1d64bcacfffaa4b949c20a758a26f614d7
Preston Darrough <techdarrough@gmail.com>
2022-02-12 12:26:51 -0800

    8th commit semi functional deck building function building a dymanic deck object

--- assets/js/blackJackapp.js
+++ assets/js/blackJackapp.js
@@ -5,7 +5,11 @@ const values = ["2","3","4","5","6","7","8","9","J","Q","K","A"];
 let deck = new Array();
 
 
-//fucntion to create deck
+
+
+
+
+// function  to create deck
 
 function createDeck () {
 //clears previous array data 
@@ -13,14 +17,20 @@ function createDeck () {
     //for each loops to populate deck array logic refactored from https://www.thatsoftwaredude.com/content/6417/how-to-code-blackjack-using-javascript
     values.forEach(value => {
         suits.forEach((suit) => {
-            // sets wieght var to be able to assign nurmeric value to all cards
+            // sets weight var to be able to assign nurmeric value to all cards
             let weight = parseInt(values[value]);
             // ternary operators conditionals used to set value of faces cards 
-            values[value] == "J" || values[value] == "Q" || values[value] == "K" ? weight = 10 : (values[value] == "A") ? weight = 11; 
+
+        //    values[value] == 'J' || values[value] == 'Q' || values[value] == 'K' ? (weight = '10') : values[value] == 'A' ? (weight = '11') ;
+    //   couldn't get it to work going back to if statment
+            if (value[values] === 'J' || values[value] === 'Q' || values[value] === 'K') {
+                weight = 10
+            } else if (values[value] === 'A') {weight = 11} else {weight = values[value]};
             let card = { Value: values[value], Suit: suits[suit], Weight: weight  };
+
             deck.push(card);
             console.log(card)
-            console.log(deck)
+           
 
             
 
@@ -35,7 +45,7 @@ function createDeck () {
 
 }
 createDeck();
-console.log(deck)
+// console.log(deck)
 
 
 
