4323a88e5ccf71fca8ef73cfe0a0c1bb4e80da1e
Preston Darrough <techdarrough@gmail.com>
2022-02-12 11:38:12 -0800

    5th commit: corrected spelling error in my var that was driving me crazy

--- assets/js/blackJackapp.js
+++ assets/js/blackJackapp.js
@@ -1,7 +1,7 @@
 
 // code inspiration from that software dude content 6417 
 const suits = ["Spade", "Clubs", "Hearts", "Diamonds"]
-const vlaues = ["2","3","4","5","6","7","8","9","J","Q","K","A"];
+const values = ["2","3","4","5","6","7","8","9","J","Q","K","A"];
 let deck = new Array();
 
 
@@ -16,10 +16,11 @@ function createDeck () {
             // sets wieght var to be able to assign nurmeric value to all cards
             let weight = parseInt(values[value]);
             // ternary operators conditionals used to set value of faces cards 
-            let conditional = values[value] == "J" || values[value] == "Q" || values[value] == "K" ? weight = 10 : (values[value] == "A") ? weight = "11" 
+            values[value] == "J" || values[value] == "Q" || values[value] == "K" ? weight = 10 : (values[value] == "A") ? weight = 11; 
             let card = { Value: values[value], Suit: suits[suit], Weight: weight  };
-            deck.push(card):
+            deck.push(card);
             console.log(card)
+            console.log(deck)
 
             
 
