0ea3c822d394dd804b282dd1f98d38ecebb9078a
Preston Darrough <techdarrough@gmail.com>
2022-02-13 11:45:49 -0800

    16th? Adding foreach shuffle function

--- assets/js/blackJackapp.js
+++ assets/js/blackJackapp.js
@@ -1,22 +1,13 @@
-
 // code inspiration from that software dude content from https://www.thatsoftwaredude.com/content/6417/how-to-code-blackjack-using-javascript
-
 const suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
 const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 let deck = new Array();
-
 // function  to create deck
-
 function createDeck () {
-//clears previous array data 
-    deck = new Array();
-    //for each loops to populate deck array
-    values.forEach(value => {suits.forEach((suit) => {
-        let weight = parseInt(value);  // sets weight var to int to be able to assign nurmeric value to all cards
-               
-           weight = (value == 'J' || value == 'Q' || value == 'K') ? weight = 10 :  (value == 'A') ? weight = 11:  value // ternary operators conditionals used to set value of faces cards 
-            //   weight = (value === "J" || value === "Q"  || value === "K"  ?  10 
-            //     :  value ===  "A"  ?  11 
+    deck = new Array();    //clears previous array data 
+    values.forEach(value => {suits.forEach((suit) => {      //for each loops to populate deck array
+    let weight = parseInt(value);  // sets weight var to int to be able to assign nurmeric value to all cards
+    weight = (value === 'J' || value == 'Q' || value === 'K') ? weight = 10 :  (value === 'A') ? weight = 11:  value // ternary operators conditionals used to set value of faces cards 
             // if (value === "J" || value=== "Q" || value === "K")  {weight = 10;} else if (value === "A")  {weight = 11}  ;  //   couldn't get the ternary work going back to if statment
             let card = { Value: value, Suit: suit, Weight: weight  };
             deck.push(card); // pop  card into deck array
@@ -24,9 +15,20 @@ function createDeck () {
     });
 }
 createDeck();
-console.log(deck)
+// console.log(deck)
 
 
 
-
+//create function to shuffle deck 
+//
+function shuffleDeck() {
+    deck.reverse().forEach((array, i) =>{
+        console.log(array, i)
+  
+        // console.log(indexPosition1)
+        // console.log(indexPosition2)
+    } )
+    
+}
+shuffleDeck()
 
