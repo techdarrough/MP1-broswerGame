6dec0af1cd427b352731a327bec0de6bfb4f645f
Preston Darrough <techdarrough@gmail.com>
2022-02-21 16:28:56 -0800

    more styling - added nav bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-const blackJackBtn = document.createEletment("input")
+let btn = document.querySelector('#atMain')
+
+
+function notify() {
+ let update = document.querySelector('#notice')
+update.innerHTML = `<p> You are already on the main page`
+}
+btn.addEventListener("click", notify)
