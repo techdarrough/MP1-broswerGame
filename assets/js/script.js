4c4da702d1678e8e987206de423c1ae3bb7e7c59
Preston Darrough <techdarrough@gmail.com>
2022-02-22 15:20:19 -0800

    more style adjustments

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,6 @@ let btn = document.querySelector('#atMain')
 
 function notify() {
  let update = document.querySelector('#notice')
-update.innerHTML = `<p> You are already on the main page`
+update.innerHTML = `<p> You are already on the main page </p>`
 }
 btn.addEventListener("click", notify)
